f0ee76ba9909faefdcbc0872c4ec4968873dd2c4
elenabaskin <ellebaskin@gmail.com>
2022-10-10 17:25:12 -0400

    added comments

--- homepage/index.js
+++ homepage/index.js
@@ -2,7 +2,7 @@
 function displayNav() {
   var nav = document.getElementById("topNav");
   if (nav.className === "navigation") {
-    nav.className += " responsive";
+    nav.className += " responsive"; //makes the nav bar go into the 3 bars instead of being written across
   } else {
     nav.className = "navigation";
   }
@@ -25,21 +25,22 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i; //i for index
-  let slides = document.getElementsByClassName("slides"); //slides are now a variable
-  let dots = document.getElementsByClassName("dot"); //dots are not a variable
-  //if statements
+  let slides = document.getElementsByClassName("slides"); //slides are being pulled from html
+  let dots = document.getElementsByClassName("dot"); //dots are also being pulled
+  //if statements for user to change slides
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
     slideIndex = slides.length;
   }
-  //for statement to only display one slide
+  //for loop
   for (i = 0; i < slides.length; i++) {
+    //every time index is less than # of slides, index goes up by 1
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", ""); //changes color of dot corresponding with slide #
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
